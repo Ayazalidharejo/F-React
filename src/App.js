8f8dd162e8ee26d0510bf31a4551a1b063d57a55
Ayazalidharejo <ayazali.xwave@gmail.com>
2024-09-21 16:04:12 +0500

    changing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import { Body } from './bodysection';
 import { Footer } from './footer';
 import { Header } from './header';
+import Section2 from './section2';
 
 function App() {
   return (
     <div>
+      <Section2 />
       <Header />
       <Body/>
     <Footer />
+    
     </div>
   );
 }
---
+++ src/section2.js
@@ -0,0 +1,8 @@
+const Section2= ()=>{
+    return(
+        <>
+        <h2> Allah will Help us to complating this cours </h2>
+        </>
+    )
+}
+export default Section2
